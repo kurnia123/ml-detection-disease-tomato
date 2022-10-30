b3ef338f0b2e0d1a7b8269db59c40b101c92dce3
kurnia123 <rizkykurnia219@gmail.com>
2022-10-30 20:56:18 +0700

    coba-1

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ class_dict = {
 
 def predict_label(img_path):
     loaded_img = load_img(img_path, target_size=(256, 256))
-    # img_array = img_to_array(loaded_img) / 255.0
-    # img_array = expand_dims(img_array, 0)
+    img_array = img_to_array(loaded_img) / 255.0
+    img_array = expand_dims(img_array, 0)
     # predicted_bit = np.round(model.predict(img_array)[0][0]).astype('int')
     return 'ada'
 
