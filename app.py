8be893e07c2eebff2a53d1699c49fec0ff4a3f71
kurnia123 <rizkykurnia219@gmail.com>
2022-10-30 10:09:32 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, Response
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from tensorflow import expand_dims
@@ -37,9 +37,10 @@ def index():
             img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(img_path)
             prediction = predict_label(img_path)
-            return render_template('index.html', uploaded_image=image.filename, prediction=prediction)
+            # return render_template('index.html', uploaded_image=image.filename, prediction=prediction)
+            return prediction
 
-    return render_template('index.html')
+    return 'kosong'
 
 @app.route('/display/<filename>')
 def send_uploaded_image(filename=''):
